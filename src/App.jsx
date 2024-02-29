3ab63b955699f8679a55d935f302936199561011
Vinod Kumar <vk5241415@gmail.com>
2024-02-29 21:33:22 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from './Pages/Home/Home'
 import {Routes,Route ,BrowserRouter as Router} from 'react-router-dom'
-import AgentList from '../agent'
 
 const App = () => {
   return (
@@ -9,7 +8,6 @@ const App = () => {
 <Router>
   <Routes>
   <Route path="/referral/:referralToken" element={<Home/>} />
-    <Route path='/a'  exact element={<AgentList/>}/>
 
   </Routes>
 </Router>
